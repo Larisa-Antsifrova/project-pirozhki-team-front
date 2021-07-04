f576888b28535080b05d1c71b291ffd41738644b
AlexBelozertsev <kashim983@gmail.com>
2021-07-04 23:22:55 +0300

    add icons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navigation.scss';
+import sprite from '../../images/sprite.svg';
 
 export default function Navigation() {
   return (
     <nav>
-      <NavLink to="/home" exact className="link" activeClassName="activeLink">
-        Главная
-      </NavLink>
-
       <NavLink
-        to="/diagram"
+        to="/dashboard/home"
         exact
         className="link"
         activeClassName="activeLink"
       >
-        Статистика
+        <svg class="icon-navifation" width="38" height="38">
+          <use href={sprite + '#main-page-mobile-icon'} />
+        </svg>
+        <span className="description">Главная</span>
+      </NavLink>
+
+      <NavLink
+        to="/dashboard/diagram"
+        exact
+        className="link"
+        activeClassName="activeLink"
+      >
+        <svg class="icon-navifation" width="38" height="38">
+          <use href={sprite + '#statistics-page-mobile-icon'} />
+        </svg>
+        <span className="description">Статистика</span>
+      </NavLink>
+      <NavLink
+        to="/dashboard/currency"
+        exact
+        className="link"
+        activeClassName="activeLink"
+      >
+        <svg class="icon-navifation" width="38" height="38">
+          <use href={sprite + '#currency-page-mobile-icon'} />
+        </svg>
       </NavLink>
     </nav>
   );
