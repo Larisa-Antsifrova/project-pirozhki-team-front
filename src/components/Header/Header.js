623c2546f25e63b989886c1671e9cd90ba77b471
arestus <arestus@gmail.com>
2021-07-06 08:28:05 +0300

    Added a transition by clicking on the logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,24 +2,27 @@ import React from 'react';
 import './Header.scss';
 import sprite from '../../images/sprite.svg';
 import { useMediaPredicate } from 'react-media-hook';
+import { Link } from 'react-router-dom';
 import ModalLogout from '../ModalLogout/ModalLogout';
 
 const Header = () => {
   const biggerThan767 = useMediaPredicate('(min-width: 768px)');
   return (
     <header className="header">
-      <span className="headerLogo">
-        {biggerThan767 ? (
-          <svg>
-            <use href={sprite + '#wallet-icon'} />
-          </svg>
-        ) : (
-          <svg>
-            <use href={sprite + '#wallet-mobile-icon'} />
-          </svg>
-        )}
-      </span>
-      <span className="headerTitle">Wallet</span>
+      <Link className="headerWrapper" to="/dashboard/home">
+        <span className="headerLogo">
+          {biggerThan767 ? (
+            <svg>
+              <use href={sprite + '#wallet-icon'} />
+            </svg>
+          ) : (
+            <svg>
+              <use href={sprite + '#wallet-mobile-icon'} />
+            </svg>
+          )}
+        </span>
+        <span className="headerTitle">Wallet</span>
+      </Link>
       <ModalLogout />
     </header>
   );
