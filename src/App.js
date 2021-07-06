202974de522f3862a0d1a396b2d2632acac7fec4
AlexBelozertsev <kashim983@gmail.com>
2021-07-06 17:02:21 +0300

    fix notice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Spinner from './components/Spinner';
-import Container from './components/Container';
 
 const DashboardPage = lazy(() => import('./pages/dashboardPage'));
 const RegisterPage = lazy(() => import('./pages/registerPage'));
@@ -10,16 +9,14 @@ const ErrorPage = lazy(() => import('./pages/errorPage'));
 
 const App = () => {
   return (
-    <Container>
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/" component={ErrorPage} />
-        </Switch>
-      </Suspense>
-    </Container>
+    <Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/" component={ErrorPage} />
+      </Switch>
+    </Suspense>
   );
 };
 export default App;
