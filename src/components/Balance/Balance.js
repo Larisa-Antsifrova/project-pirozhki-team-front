3f6b68354198a63987703d8f87c3b5d6753762b6
arestus <arestus@gmail.com>
2021-07-07 10:58:22 +0300

    Added markup and styles for Balance-component

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Balance.scss';
 
-const Balance = () => (
-  <div className="balance">
-    <p className="balanceTitle">Ваш баланс</p>
-    <p className="balanceTotal">
-      <span className="balanceSymbol">₴</span> 24 000.00
-    </p>
-  </div>
-);
-
+const Balance = () => {
+  const balance = useSelector(state => state.finance.totalBalance);
+  return (
+    <div className="balance">
+      <p className="balanceTitle">Ваш баланс</p>
+      <p className="balanceTotal">
+        <span className="balanceSymbol">₴</span> {balance}
+      </p>
+    </div>
+  );
+};
 export default Balance;
