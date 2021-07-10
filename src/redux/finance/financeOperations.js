6467d87ed7d13d7a7956a3bca3ae30c872a243d9
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-10 21:15:08 +0300

    add operation for statistic and filter to date

--- src/redux/finance/financeOperations.js
+++ src/redux/finance/financeOperations.js
@@ -13,5 +13,39 @@ const totalBalance = () => async dispatch => {
   }
 };
 
+const statistics = (month, year) => async dispatch => {
+  dispatch(financeActions.statisticsRequest());
+
+  function getDaysInMonth(y, m) {
+    return m === 2
+      ? y & 3 || (!(y % 25) && y & 15)
+        ? 28
+        : 29
+      : 30 + ((m + (m >> 3)) & 1);
+  }
+
+  const today = new Date();
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+
+  const startDate = `${year ? year : yyyy}-${month ? month : mm}-01`;
+  const endDate = `${year ? year : yyyy}-${month ? month : mm}-${getDaysInMonth(
+    +`${year ? year : yyyy}`,
+    +`${month ? month : mm}`,
+  )}`;
+
+  console.log(startDate);
+  console.log(endDate);
+  try {
+    const { data } = await axios.get(
+      `/statistics?startDate=${startDate}&endDate=${endDate}`,
+    );
+
+    dispatch(financeActions.statisticsSuccess(data.data));
+  } catch (error) {
+    dispatch(financeActions.statisticsError());
+  }
+};
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { totalBalance };
+export default { totalBalance, statistics };
