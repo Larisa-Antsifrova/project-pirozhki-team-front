cad092a6242abcb9d4d92c9c1a06979117eb75c8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-13 00:55:04 +0300

    Added conditional hero rendering with media-hook.

--- src/pages/registerPage/registerPage.js
+++ src/pages/registerPage/registerPage.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useMediaPredicate } from 'react-media-hook';
 import Container from '../../components/Container';
 import RegistrationForm from '../../components/RegistrationForm';
 import './registerPage.scss';
 
 const RegisterPage = () => {
+  const biggerThan767 = useMediaPredicate('(min-width: 768px)');
+
   return (
     <main>
       <div className="authWrapper">
         <Container>
-          <div className="heroWrapper">
-            <div className="heroImageRegister"></div>
-            <div className="heroTitle">
-              <h1 className="heroTitleText">Finance App</h1>
+          {biggerThan767 ? (
+            <div className="heroWrapper">
+              <div className="heroImageRegister"></div>
+              <div className="heroTitle">
+                <h1 className="heroTitleText">Finance App</h1>
+              </div>
             </div>
-          </div>
+          ) : null}
           <div className="formWrapper">
             <RegistrationForm />
           </div>
