f7e16c9d7d1034c7135d4cd910a9aa533a8d9c2b
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-15 00:02:53 +0300

    Moved fetch function to useEffect.

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import './Currency.scss';
 import axios from 'axios';
+import './Currency.scss';
 
 const Currency = () => {
   const [error, setError] = useState(null);
   const [currency, setCurrency] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
 
-  async function getCurrency() {
-    try {
-      let response = await axios.get(
-        'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
-      );
-      const result = await response.data;
-      setIsLoaded(true);
-      setCurrency(result);
-    } catch (e) {
-      setIsLoaded(true);
-      setError(error);
-    }
-  }
-
   useEffect(() => {
+    async function getCurrency() {
+      try {
+        let response = await axios.get(
+          'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5',
+        );
+        const result = await response.data;
+        setIsLoaded(true);
+        setCurrency(result);
+      } catch (e) {
+        setIsLoaded(true);
+        setError(error);
+      }
+    }
+
     getCurrency();
-  }, []);
+  }, [error]);
 
   const currencyFiltered = currency.filter(curr => {
     return curr.ccy !== 'BTC';
