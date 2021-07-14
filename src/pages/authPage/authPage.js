f93eb4a6396c49d1ac11ce70d4011b66a3558c43
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-14 23:09:57 +0300

    Removed comments.

--- src/pages/authPage/authPage.js
+++ src/pages/authPage/authPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Switch, Redirect, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { useMediaPredicate } from 'react-media-hook';
-import WithAuthRedirect from '../../components/WithAuthRedirect';
 import Container from '../../components/Container';
 import RegistrationForm from '../../components/RegistrationForm';
 import LoginForm from '../../components/LoginForm';
@@ -9,19 +8,11 @@ import './authPage.scss';
 
 const AuthPage = () => {
   const currentPage = useLocation();
-
-  console.log('location', currentPage.pathname);
-
   const biggerThan767 = useMediaPredicate('(min-width: 768px)');
   const biggerThan1280 = useMediaPredicate('(min-width: 1280px)');
 
   return (
     <>
-      {/* {currentPage.pathname === '/' ? (
-        <Redirect to="/auth/login" />
-      ) : (
-        <Redirect to="/auth/register" />
-      )} */}
       <main>
         <div className="authWrapper">
           {biggerThan1280 ? <div className="authBackdrop"></div> : null}
