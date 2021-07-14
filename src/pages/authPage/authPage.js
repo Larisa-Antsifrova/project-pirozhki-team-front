02caee8eebb1464148debb46798830ad529393f2
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-14 22:33:05 +0300

    Removed register and login files.

--- src/pages/authPage/authPage.js
+++ src/pages/authPage/authPage.js
@@ -1,82 +1,59 @@
 import React from 'react';
-// import React, { lazy } from 'react';
 import { Switch, Redirect, useLocation } from 'react-router-dom';
-// import { useLocation } from 'react-router-dom';
-// import WithAuthRedirect from '../../components/WithAuthRedirect';
-
 import { useMediaPredicate } from 'react-media-hook';
+import WithAuthRedirect from '../../components/WithAuthRedirect';
 import Container from '../../components/Container';
 import RegistrationForm from '../../components/RegistrationForm';
 import LoginForm from '../../components/LoginForm';
-import '../registerPage/registerPage.scss';
-
-// const RegisterPage = lazy(() => import('../registerPage'));
-// const LoginPage = lazy(() => import('../loginPage'));
+import './authPage.scss';
 
 const AuthPage = () => {
-  const location = useLocation();
-  console.log(location.pathname);
+  const currentPage = useLocation();
+
+  console.log('location', currentPage.pathname);
 
   const biggerThan767 = useMediaPredicate('(min-width: 768px)');
   const biggerThan1280 = useMediaPredicate('(min-width: 1280px)');
 
   return (
-    <main>
-      <Redirect to="/auth/login" />
-      <div className="authWrapper">
-        {biggerThan1280 ? <div className="authBackdrop"></div> : null}
-        <Container>
-          <div className="authContainer">
-            {biggerThan767 ? (
-              <div className="heroWrapper">
-                <div
-                  className={
-                    location.pathname === '/auth/login'
-                      ? 'heroImageLogin'
-                      : 'heroImageRegister'
-                  }
-                ></div>
-                <div className="heroTitle">
-                  <h1 className="heroTitleText">Finance App</h1>
+    <>
+      {/* {currentPage.pathname === '/' ? (
+        <Redirect to="/auth/login" />
+      ) : (
+        <Redirect to="/auth/register" />
+      )} */}
+      <main>
+        <div className="authWrapper">
+          {biggerThan1280 ? <div className="authBackdrop"></div> : null}
+          <Container>
+            <div className="authContainer">
+              {biggerThan767 ? (
+                <div className="heroWrapper">
+                  <div
+                    className={
+                      currentPage.pathname === '/auth/login'
+                        ? 'heroImageLogin'
+                        : 'heroImageRegister'
+                    }
+                  ></div>
+                  <div className="heroTitle">
+                    <h1 className="heroTitleText">Finance App</h1>
+                  </div>
                 </div>
+              ) : null}
+              <div className="formWrapper">
+                {currentPage.pathname === '/auth/login' ? (
+                  <LoginForm />
+                ) : (
+                  <RegistrationForm />
+                )}
               </div>
-            ) : null}
-            <div className="formWrapper">
-              {location.pathname === '/auth/login' ? (
-                <LoginForm />
-              ) : (
-                <RegistrationForm />
-              )}
             </div>
-          </div>
-        </Container>
-      </div>
-    </main>
+          </Container>
+        </div>
+      </main>
+    </>
   );
-
-  // return (
-  //   <>
-  //     <Switch>
-  //       <WithAuthRedirect
-  //         path="/auth/login"
-  //         type="guest"
-  //         redirectTo="/dashboard/home"
-  //       >
-  //         <LoginPage />
-  //       </WithAuthRedirect>
-
-  //       <WithAuthRedirect
-  //         path="/auth/register"
-  //         type="guest"
-  //         redirectTo="/dashboard/home"
-  //       >
-  //         <RegisterPage />
-  //       </WithAuthRedirect>
-
-  //       <Redirect to="/auth/login" />
-  //     </Switch>
-  //   </>
-  // );
 };
 
 export default AuthPage;
