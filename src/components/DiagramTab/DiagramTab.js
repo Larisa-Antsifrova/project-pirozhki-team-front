a5e91f7ed824e27f96d3e7be4f5c0b7021434a62
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-15 11:31:20 +0300

    added dynamic year for select

--- src/components/DiagramTab/DiagramTab.js
+++ src/components/DiagramTab/DiagramTab.js
@@ -5,6 +5,8 @@ import {
   getStatistics,
   costsIncomeTotals,
 } from '../../redux/finance/financeSelectors';
+import { getRegisterDataUser } from '../../redux/auth/authSelectors';
+import { getYear } from '../../helpers/operation';
 import operations from '../../redux/finance/financeOperations';
 import { MONTH, NO_TRANSACTION } from '../../helpers/constants';
 import Spinner from '../Spinner';
@@ -13,20 +15,39 @@ import Table from '../Table';
 import SelectMonthYear from '../SelectMonthYear';
 import './DiagramTab.scss';
 
-const year = [2020, 2021, 2023];
+const year = [];
 
 const DiagramTab = () => {
+  const [seletcMonth, setSeletcMonth] = useState(null);
+  const [seletcYear, setSeletcYear] = useState(null);
+
   const dispatch = useDispatch();
   const statistics = useSelector(getStatistics);
   const costsIncome = useSelector(costsIncomeTotals);
   const isLoading = useSelector(getIsLoading);
-
-  const [seletcMonth, setSeletcMonth] = useState(null);
-  const [seletcYear, setSeletcYear] = useState(null);
+  const registerDateUser = useSelector(getRegisterDataUser);
+  const registerYearUser = getYear(new Date(registerDateUser));
 
   useEffect(() => {
     dispatch(operations.statistics(seletcMonth, seletcYear));
   }, [dispatch, seletcYear, seletcMonth]);
+
+  useEffect(() => {
+    const registerYear = registerYearUser;
+    const todayYear = getYear(new Date());
+    const difference = todayYear - registerYear;
+    const setYear = () => {
+      year.push(registerYear);
+      if (registerYear === todayYear) {
+        return;
+      }
+      for (let i = 1; i <= difference; i++) {
+        year.push(registerYear + i);
+      }
+    };
+
+    setYear();
+  }, [registerYearUser]);
 
   const onSelectMonth = itemTitle => {
     const monthNum = MONTH.indexOf(itemTitle) + 1;
