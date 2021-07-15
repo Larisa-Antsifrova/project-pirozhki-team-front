32f120cf4976ac5ec5dcc29b99808fd6f4946c39
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-15 11:22:23 +0300

    added change of import and export from default to named

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,6 +1,2 @@
-const getIsAuthenticated = state => state.auth.isAuth;
-const getUsername = state => state.auth.user.name;
-const getUserEmail = state => state.auth.user.email;
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getIsAuthenticated, getUsername, getUserEmail };
+export const getIsAuthenticated = state => state.auth.isAuth;
+export const getRegisterDataUser = state => state.auth?.user?.data?.createdAt;
