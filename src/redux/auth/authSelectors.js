effed85a9618232e7e39f2b3a4d6a9963a3319f4
arestus <arestus@gmail.com>
2021-07-15 18:59:21 +0300

    Code refactoring

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -4,7 +4,6 @@ export const getUserName = state =>
     ? state.auth.user.data.name
     : state.auth.user.name;
 export const getUserEmail = state => state.auth.user.email;
-export const getUserNameRefresh = state => state.auth.user.data.name;
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getIsAuthenticated,
