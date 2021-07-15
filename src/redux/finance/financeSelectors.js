62eaf968ed607df7d4be1c5158a0a0f559ada7d9
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-15 11:23:41 +0300

    delete comment

--- src/redux/finance/financeSelectors.js
+++ src/redux/finance/financeSelectors.js
@@ -1,4 +1,3 @@
 export const getTotalBalance = state => state.finance.totalBalance;
 export const getStatistics = state => state.finance.statistics?.statistics;
 export const costsIncomeTotals = state => state.finance.statistics?.totals;
-// export const isLoadingStatistics = state => state.finance.isLoadingStatistics;
