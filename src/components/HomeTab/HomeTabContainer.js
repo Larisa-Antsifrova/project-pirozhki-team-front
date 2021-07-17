172321a0a8fa6c7bf027e991cb74f5413abe71fb
arestus <arestus@gmail.com>
2021-07-17 15:17:16 +0300

    Added some styles and scroll

--- src/components/HomeTab/HomeTabContainer.js
+++ src/components/HomeTab/HomeTabContainer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { transactions, totals } from '../../redux/finance/financeSelectors';
-import { getIsLoading } from '../../redux/isLoading/isLoadingSelectors';
+// import { getIsLoading } from '../../redux/isLoading/isLoadingSelectors';
 
 import ButtonAddTransactions from '../ButtonAddTransactions';
 import HomeTab from './HomeTab';
-import Spinner from '../Spinner';
+// import Spinner from '../Spinner';
 import cn from 'classnames';
 
 import './HomeTab.scss';
@@ -13,21 +13,8 @@ import './HomeTab.scss';
 const HomeTabContainer = () => {
   const transactionsList = useSelector(transactions);
   const totalList = useSelector(totals);
-  const isLoading = useSelector(getIsLoading);
-  // console.log(transactionsList);
+  console.log(transactionsList);
 
-  // const currentBalance =
-  //   transactionsList &&
-  //   transactionsList.reduce((acc, el) => {
-  //     const newArr = [];
-  //     newArr.push(el.income ? acc + el.sum : acc - el.sum);
-  //     // console.log(newArr);
-  //     // // console.log(el.income ? acc + el.sum : acc - el.sum);
-  //     // // console.log('el.sum', el.comment);
-  //     return newArr;
-  //   }, 0);
-
-  // console.log(currentBalance);
   return (
     <>
       {/* {isLoading ? (
@@ -54,22 +41,24 @@ const HomeTabContainer = () => {
             <span>Баланс</span>
           </p>
         </div>
-        {transactionsList &&
-          transactionsList.map(
-            ({ id, comment, sum, category, income, date }) => {
-              return (
-                <HomeTab
-                  totals={totalList.balance}
-                  key={id}
-                  comment={comment}
-                  sum={sum}
-                  category={category}
-                  income={income}
-                  date={date}
-                />
-              );
-            },
-          )}
+        <div className="transactionsListData">
+          {transactionsList &&
+            transactionsList.map(
+              ({ id, comment, sum, category, income, date }) => {
+                return (
+                  <HomeTab
+                    totals={totalList.balance}
+                    key={id}
+                    comment={comment}
+                    sum={sum}
+                    category={category}
+                    income={income}
+                    date={date}
+                  />
+                );
+              },
+            )}
+        </div>
       </>
       {/* )} */}
       <ButtonAddTransactions />
