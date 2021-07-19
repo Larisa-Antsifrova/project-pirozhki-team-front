2b59618f2c4e97d8e4bf854daa3402e8ba344e2b
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-19 21:58:26 +0300

    fix smal-loader position

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,13 +1,9 @@
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { numberToLocalString } from '../../helpers/operation';
-import { isLoadingStatistic } from '../../redux/finance/financeSelectors';
-import SmallSpinner from '../SmallSpinner';
 import './Table.scss';
 
 const Table = ({ tempData, costsIncome }) => {
-  const isLoading = useSelector(isLoadingStatistic);
   const data = useMemo(() => tempData, [tempData]);
   const { expense, income } = useMemo(() => costsIncome, [costsIncome]);
 
@@ -20,44 +16,38 @@ const Table = ({ tempData, costsIncome }) => {
             <th className="tableHeaderSum">Сумма</th>
           </tr>
         </thead>
-        {isLoading ? (
-          <div className="diagramTabSpinner">
-            <SmallSpinner color={'#4a56e2'} size={80} />
-          </div>
-        ) : (
-          <tbody>
-            {data.map(({ category, sum, color }) => (
-              <tr key={category}>
-                <td className="tableCategories">
-                  <span
-                    className="tableCategoriesColor"
-                    style={{
-                      backgroundColor: `${color}`,
-                    }}
-                  ></span>
-                  {category}
-                </td>
-                <td className="tableSum">
-                  {Number.isInteger(sum)
-                    ? `${sum}.00`
-                    : Math.round(sum * 100) / 100}
-                </td>
-              </tr>
-            ))}
-            <tr className="tableCostsIncome">
-              <td>Расходы:</td>
-              <td className="tableCostsValue tableSum">
-                {numberToLocalString(expense)}
+        <tbody>
+          {data?.map(({ category, sum, color }) => (
+            <tr key={category}>
+              <td className="tableCategories">
+                <span
+                  className="tableCategoriesColor"
+                  style={{
+                    backgroundColor: `${color}`,
+                  }}
+                ></span>
+                {category}
+              </td>
+              <td className="tableSum">
+                {Number.isInteger(sum)
+                  ? `${sum}.00`
+                  : Math.round(sum * 100) / 100}
               </td>
             </tr>
-            <tr className="tableCostsIncome">
-              <td>Доходы:</td>
-              <td className="tableIncomeValue tableSum">
-                {numberToLocalString(income)}
-              </td>
-            </tr>
-          </tbody>
-        )}
+          ))}
+          <tr className="tableCostsIncome">
+            <td>Расходы:</td>
+            <td className="tableCostsValue tableSum">
+              {numberToLocalString(expense)}
+            </td>
+          </tr>
+          <tr className="tableCostsIncome">
+            <td>Доходы:</td>
+            <td className="tableIncomeValue tableSum">
+              {numberToLocalString(income)}
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
