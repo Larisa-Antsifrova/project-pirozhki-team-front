09037a13723ec938d018139157e832d3ebff40a8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-21 20:57:16 +0300

    Edited typo.

--- src/http/api-axios.js
+++ src/http/api-axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8888/';
+export const API_URL = 'http://localhost:8888';
 // export const API_URL = 'https://awesome-wallet-app.herokuapp.com';
 
 const $api = axios.create({ withCredentials: true, baseURL: API_URL });
@@ -16,7 +16,7 @@ $api.interceptors.response.use(
   },
   async error => {
     console.log('Intercepter for Response is triggered');
-    const originalRequest = error.congif;
+    const originalRequest = error.config;
 
     if (
       error.response.status === 401 &&
@@ -25,9 +25,12 @@ $api.interceptors.response.use(
     ) {
       originalRequest._isRetry = true;
       try {
+        console.log('Inside TRY');
         const { data } = await axios.get(`${API_URL}/auth/refresh`, {
           withCredentials: true,
         });
+
+        console.log('data in interceptor', data);
 
         const accessToken = data.data.accessToken;
         localStorage.setItem('token', accessToken);
