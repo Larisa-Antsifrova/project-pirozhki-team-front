d6f375a5222af9bb62cb2e2ee0551af6940f14af
Kramskiy Dima <kramskiyd@gmail.com>
2021-07-22 20:54:45 +0300

    added selector for isLoadingTransaction

--- src/redux/finance/financeSelectors.js
+++ src/redux/finance/financeSelectors.js
@@ -4,5 +4,6 @@ export const pagination = state => state.finance.transactions.pagination;
 export const getStatistics = state => state.finance.statistics?.statistics;
 export const costsIncomeTotals = state => state.finance.statistics?.totals;
 export const isLoadingStatistic = state => state.finance.isLoadingStatistic;
+export const isLoadingTransaction = state => state.finance.isLoadingTransaction;
 export const getFirstTransactionDate = state =>
   state.finance.statistics?.earliest;
